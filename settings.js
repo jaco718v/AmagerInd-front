78d6a68bb552b1fbe4c3586770a97dabe8cd820e
jaco718v <100360047+jaco718v@users.noreply.github.com>
2023-05-22 15:42:14 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://amagerrecordsbackend.azurewebsites.net/api/"
+export const API_URL = "https://amasetindi.azurewebsites.net/api/"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
 
